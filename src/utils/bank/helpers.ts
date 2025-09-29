43bcdf56caed6d5123d3ae2a0379d825e16773c3
hgg <gksrhkdrn44@naver.com>
2025-09-29 20:07:36 +0900

    임시커밋

--- src/utils/bank/helpers.ts
+++ src/utils/bank/helpers.ts
@@ -4,8 +4,7 @@ export const getProblemTypeInKorean = (type: string): string => {
   switch (type.toLowerCase()) {
     case ProblemType.MULTIPLE_CHOICE:
       return '객관식';
-    case ProblemType.ESSAY:
-      return '서술형';
+
     case ProblemType.SHORT_ANSWER:
       return '단답형';
     default:
